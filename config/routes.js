aaa86b152b2e97a65cd33cda6e2bb0a77a1bfe1d
aheaton <aliciajheaton@gmail.com>
2017-11-05 14:14:04 -0500

    Add delete functionality to BE only
    - Add delete method to controller
    - Test via Postman

--- config/routes.js
+++ config/routes.js
@@ -9,7 +9,7 @@ module.exports = require('lib/wiring/routes')
 
 // standards RESTful routes
 .resources('examples')
-.resources('uploads', { only: ['index', 'show', 'create'] })
+.resources('uploads', { only: ['index', 'show', 'create', 'destroy', 'update'] })
 
 // users of the app have special requirements
 .post('/sign-up', 'users#signup')
