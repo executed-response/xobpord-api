93b4e0edc7f3982129423746e7b6d022a151e0ff
aheaton <Pappy123!>
2017-11-05 11:54:52 -0500

    Start setting up uploads resource
    - Add routes for index and create
    - Set up schema
    - Test index is passing object via localhost
    - Set up create, hard coding values in object
    - Test create is sending 201 response via Postman
    
    Alicia, Sam, John

---
+++ app/controllers/uploads.js
@@ -0,0 +1,103 @@
+'use strict'
+//
+// const multer = require('multer')
+// const multerUpload = multer({dest: '/tmp'})
+
+const controller = require('lib/wiring/controller')
+const Upload = require('app/models/uploads')
+// const s3Upload = require('lib/aws-s3-upload')
+
+const authenticate = require('./concerns/authenticate')
+const setUser = require('./concerns/set-current-user')
+const setModel = require('./concerns/set-mongoose-model')
+
+const index = (req, res, next) => {
+  Upload.find()
+    .then(uploads => res.json({
+      uploads: uploads.map((e) =>
+        e.toJSON({ virtuals: true, user: req.user }))
+    }))
+    .catch(next)
+}
+
+const show = (req, res) => {
+  res.json({
+    upload: req.upload.toJSON({ virtuals: true, user: req.user })
+  })
+}
+
+const create = (req, res, next) => {
+  const upload = {
+    filename: 'filename test',
+    description: 'description test',
+    url: 'url test',
+    tags: 'tag test',
+    _owner: req.user._id
+  }
+  Upload.create(upload)
+  .then(() => {
+    res.status(201).json()
+  })
+  .catch(next)
+}
+
+  // const file = {
+  //   path: req.file.path,
+  //   name: req.file.originalname,
+  //   mimetype: req.file.mimetype
+  // }
+  // const upload = Object.assign(req.body.upload, {
+  //   _owner: req.user._id
+  // })
+
+  // something along these lines? How do I get the file path and name?
+  // s3Upload(file)
+  //   .then((s3Response) => Upload.create({ // .create does return a promise so we don't need to promisify it ourselves; single line functions have implicit returns so don't need offical "return" word in there (if there were curly brackets, then you would need "return")
+  //     description: 'hard code me first',
+  //     url: s3Response.Location
+  //   }))
+  //   .then((upload) => {
+  //     return res.status(201)
+  //         .json({
+  //           upload: upload
+  //         })
+  //   })
+  //         .catch(next)
+
+//  req.body is { image: { title: '' } }
+// req.file is { fieldname: 'image[file]',
+//   originalname: 'TicTacToe-Wireframes.jpg',
+//   encoding: '7bit',
+//   mimetype: 'image/jpeg',
+//   destination: '/tmp',
+//   filename: '04d79db8979c862298b488c278a04c73',
+//   path: '/tmp/04d79db8979c862298b488c278a04c73',
+//   size: 6235651 }
+
+const update = (req, res, next) => {
+  delete req.body.upload._owner  // disallow owner reassignment.
+
+  req.upload.update(req.body.upload)
+    .then(() => res.sendStatus(204))
+    .catch(next)
+}
+
+const destroy = (req, res, next) => {
+  req.upload.remove()
+    .then(() => res.sendStatus(204))
+    .catch(next)
+}
+
+module.exports = controller({
+  index,
+  show,
+  create,
+  update,
+  destroy
+}, { before: [
+  { method: setUser, only: ['index', 'show'] },
+  // { method: multerUpload.single('image[file]'), only: ['create'] }, // this creates req.file
+  { method: authenticate, except: ['index', 'show'] },
+  { method: setModel(Upload), only: ['show'] },
+  { method: setModel(Upload, { forUser: true }), only: ['update', 'destroy'] }
+] })
---
+++ app/models/uploads.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+const uploadSchema = new mongoose.Schema({
+  filename: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  tags: {
+    type: String
+  },
+  _owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true,
+  toJSON: {
+    transform: function (doc, ret, options) {
+      const userId = (options.user && options.user._id) || false
+      ret.editable = userId && userId.equals(doc._owner)
+      return ret
+    }
+  }
+})
+
+const Upload = mongoose.model('Upload', uploadSchema)
+
+module.exports = Upload
